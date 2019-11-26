e6f65b08922820254c5bc54ae0bb0bb87d2a4ff5
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-26 21:30:33 +0700

    short assignment

---
+++ short_assignment.rb
@@ -0,0 +1,17 @@
+a = 10
+b = 4
+
+puts "B = #{b}"
+puts "A = #{a}"
+a += b
+puts "+ B = #{a}"
+a -= b
+puts "- B = #{a}"
+a *= b
+puts "* B = #{a}"
+a /= b
+puts "/ B = #{a}"
+a **= b
+puts "** B = #{a}"
+a %= b
+puts "mod B = #{a}"
